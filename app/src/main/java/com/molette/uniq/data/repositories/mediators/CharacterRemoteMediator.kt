96e77a10eb7d9d9afef5241d7fe80f4e86cd19bd
Mathieu <mathieumolette@gmail.com>
2020-09-30 16:09:09 +0200

    Fix paging first launch

--- app/src/main/java/com/molette/uniq/data/repositories/mediators/CharacterRemoteMediator.kt
+++ app/src/main/java/com/molette/uniq/data/repositories/mediators/CharacterRemoteMediator.kt
@@ -37,26 +37,15 @@ class CharacterRemoteMediator(
                 remoteKeys?.nextKey?.minus(PAGE_SIZE) ?: STARTING_OFFSET
             }
             LoadType.PREPEND -> {
-                val remoteKeys = getRemoteKeyForFirstItem(state)
-                if (remoteKeys == null) {
-                    // The LoadType is PREPEND so some data was loaded before,
-                    // so we should have been able to get remote keys
-                    // If the remoteKeys are null, then we're an invalid state and we have a bug
-                    throw InvalidObjectException("Remote key and the prevKey should not be null")
-                }
-                // If the previous key is null, then we can't request more data
-                val prevKey = remoteKeys.previousKey
-                if (prevKey == null) {
-                    return MediatorResult.Success(endOfPaginationReached = true)
-                }
-                remoteKeys.previousKey
+                return MediatorResult.Success(endOfPaginationReached = true)
             }
             LoadType.APPEND -> {
                 val remoteKeys = getRemoteKeyForLastItem(state)
-                if (remoteKeys == null || remoteKeys.nextKey == null) {
+                if (remoteKeys == null)
+                    STARTING_OFFSET
+                else if(remoteKeys.nextKey == null){
                     throw InvalidObjectException("Remote key should not be null for $loadType")
-                }
-                remoteKeys.nextKey
+                }else remoteKeys.nextKey
             }
         }
 
